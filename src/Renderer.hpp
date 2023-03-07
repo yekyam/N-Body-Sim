ea4da98ca2df650ea5f2dc9a2e26e5aff8130b85
manuel <mmateo@loyola.edu>
2023-03-07 02:17:36 -0500

    cleanup extra files

---
+++ src/Renderer.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+
+#include "Entity.hpp"
+#include "RGB.hpp"
+#include "Vec2d.hpp"
+
+class Renderer
+{
+	std::vector<std::vector<Entity>> frames;
+
+    public:
+};
