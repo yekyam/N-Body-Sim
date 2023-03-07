30337153201d2bd89e3484a0ac6fb4e6f5b3acba
manuel <mmateo@loyola.edu>
2023-03-07 02:29:21 -0500

    add todo + basic renderer fields

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <exception>
+#include <iostream>
+#include <stdexcept>
 #include <vector>
+
+#include <SDL2/SDL.h>
 
 #include "Entity.hpp"
 #include "RGB.hpp"
@@ -10,5 +15,23 @@ class Renderer
 {
 	std::vector<std::vector<Entity>> frames;
 
+	SDL_Window *m_window;
+	SDL_Surface *m_window_surface;
+
     public:
+	Renderer(std::vector<std::vector<Entity>> frames) : frames(frames)
+	{
+		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		{
+			throw std::runtime_error("Couldn't init SDL2?");
+		}
+
+		m_window = SDL_CreateWindow(
+		    "N-Body Gravity Sim", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1500, 800, 0);
+		if (!m_window)
+		{
+			throw std::runtime_error("Couldn't create SDL2 window?");
+		}
+		m_window_surface = SDL_GetWindowSurface(m_window);
+	}
 };
